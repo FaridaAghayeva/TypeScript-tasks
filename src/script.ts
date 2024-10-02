11f710704bc765991bef9510bb6fc5ea907c2a5b
Farida Aghayeva <farida.agayeva20@gmail.com>
2024-10-02 15:29:52 +0400

    ts tasks

---
+++ src/script.ts
@@ -0,0 +1,95 @@
+// // task 1
+// const literal: "farida" = "farida";
+
+// // task 2 ??
+
+
+// // task3
+// type strOrNum = string | number;
+
+// function handleStringOrNumber(value: strOrNum): void {
+//     if (typeof value === "string") {
+//         console.log(`String: ${value}`);
+//     } else {
+//         console.log(`Number: ${value}`);
+//     }
+// }
+
+// // task4
+
+// interface Person {
+//     name: string;
+//     age: number;
+// }
+
+// const person: Person = {
+//     name: "John Doe",
+//     age: 30
+// };
+
+// // task5
+
+// type a = number
+// type b = number
+// function addNumbers(a: number, b: number) {
+//     return a + b
+// }
+
+// // task6
+
+// const numbers: number[] = [1, 2, 3, 4, 5];
+
+// function sumArray(numbers: number[]): number {
+//     const numberArray: number[] = numbers.map(item => item + item);
+//     // return numberArray: number[]
+// }
+
+// // task7
+
+// type ErrorCode = "not found" | "too many reuqests";
+
+// function displayErrorMessage(code: ErrorCode): string {
+//     if (code === 'not found') {
+//         return "not found.";
+//     } else if (code === 'too many reuqests') {
+//         return "too many reuqest";
+//     }
+
+// }
+
+// // task8 
+
+// type Coordinate = [number, number];
+
+// const point: Coordinate = [10, 20];
+
+// // task9 
+
+// interface Product {
+//     id: number;
+//     name: string;
+//     price?: number;
+//     category?: string;
+// }
+
+// function displayProductDetails(product: Product): void {
+
+//     if (product.price) {
+//         console.log(`Price: ${product.price} Name: ${product.name}`);
+//     }
+//     if (product.category) {
+//         console.log(`Category: ${product.category}  Name: ${product.name}`);
+//     }
+// }
+
+// // task10
+
+// function displayStringLength(strings: string[]): number[] {
+//     return strings.map(str => str.length);
+// }
+
+// // task11 ??
+
+
+// // task12
+
